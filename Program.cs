7933a90a6cc4e7f16cd6a7431e366d8c62938c26
ilyasidor <63228249+ilyasidor@users.noreply.github.com>
2021-08-17 12:59:03 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.Win32.TaskScheduler;
 
 namespace threading_learning
@@ -8,7 +8,29 @@ namespace threading_learning
         static void Main(string[] args)
         {
             TaskService.Instance.AddTask("Test", QuickTriggerType.Daily, "myprogram.exe", "-a arg");
-            Console.WriteLine("Hello World!");
+            TaskManage task = new TaskManage("InformationSecurity", "SSS.exe", QuickTriggerType.Hourly);
+            task.TaskCreate();
+            TaskCrate("InformationSecurity", "SSS.exe", QuickTriggerType.Hourly);
+        }
+        public static void TaskCrate(string NAME_TASK, string PATH_TO_MY_PROGRAMM, QuickTriggerType triggerType)
+        {
+            TaskService.Instance.AddTask(NAME_TASK, triggerType, PATH_TO_MY_PROGRAMM);
+        }
+    }
+    class TaskManage
+    {
+        string PATH_TO_MY_PROGRAMM;
+        string NAME_TASK;
+        QuickTriggerType triggerType;
+        public TaskManage(string NAME_TASK, string PATH_TO_MY_PROGRAMM, QuickTriggerType triggerType)
+        {
+            this.NAME_TASK = NAME_TASK;
+            this.PATH_TO_MY_PROGRAMM = PATH_TO_MY_PROGRAMM;
+            this.triggerType = triggerType;
+        }
+        public void TaskCreate()
+        {
+            TaskService.Instance.AddTask(NAME_TASK, triggerType, PATH_TO_MY_PROGRAMM);
         }
     }
 }
