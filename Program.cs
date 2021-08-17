cd95df864a9d31c70f07d885e6a0e653fddff8c7
ilyasidor <63228249+ilyasidor@users.noreply.github.com>
2021-08-17 12:39:45 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Win32.TaskScheduler;
+
+namespace threading_learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskService.Instance.AddTask("Test", QuickTriggerType.Daily, "myprogram.exe", "-a arg");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
